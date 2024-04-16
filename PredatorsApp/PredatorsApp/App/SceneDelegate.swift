c354e13086d78b512c16b2e9512d541632fa0e36
Matvei <khlestov.matvei@mail.ru>
2024-04-16 14:05:07 +0500

    Added data model for app

--- PredatorsApp/PredatorsApp/App/SceneDelegate.swift
+++ PredatorsApp/PredatorsApp/App/SceneDelegate.swift
@@ -11,12 +11,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    func scene(_ scene: UIScene, 
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        let viewController = PredatorsViewController()
+        let viewController = UINavigationController(rootViewController: PredatorsViewController())
         window.rootViewController = viewController
         self.window = window
         window.makeKeyAndVisible()
