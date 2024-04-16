bd0a316b3e4e20e0b5ad702cd41f626ac0a6b119
Matvei <khlestov.matvei@mail.ru>
2024-04-17 03:30:35 +0500

    Added ui elements and implemented data transfer

--- PredatorsApp/PredatorsApp/Controllers/ImageViewController.swift
+++ PredatorsApp/PredatorsApp/Controllers/ImageViewController.swift
@@ -9,8 +9,31 @@ import UIKit
 
 final class ImageViewController: UIViewController {
 
+    // MARK: - Public Properties
+    var selectedImage: UIImage?
+    
+    // MARK: - UI Elements
+    private lazy var mainImageView: UIImageView = {
+        let imageView = UIImageView(
+            frame: CGRect(
+                x: 0,
+                y: 0,
+                width: view.frame.width,
+                height: 285
+            )
+        )
+        imageView.image = selectedImage
+        imageView.contentMode = .scaleAspectFill
+        imageView.center.x = view.center.x
+        imageView.center.y = view.center.y
+        
+        return imageView
+    }()
+    
+    // MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        view.addSubview(mainImageView)
     }
 }
